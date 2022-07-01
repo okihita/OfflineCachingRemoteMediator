1f2590c89cd1869ee23081e3d56274ed1ba8f954
Okihita Sihaloho <okihita@gmail.com>
2022-07-01 22:07:10 +0700

    Added UnsplashApi.kt

---
+++ app/src/main/java/com/okihita/offlinecachingremotemediator/data/remote/UnsplashApi.kt
@@ -0,0 +1,25 @@
+package com.okihita.offlinecachingremotemediator.data.remote
+
+import com.okihita.offlinecachingremotemediator.BuildConfig
+import com.okihita.offlinecachingremotemediator.model.UnsplashImage
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface UnsplashApi {
+
+    @Headers("Accept-Version: v1", "Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/photos")
+    suspend fun getAllImages(
+        @Query("page") page: Int,
+        @Query("per_page") per_page: Int
+    ): List<UnsplashImage>
+
+    @Headers("Accept-Version: v1", "Authorization: Client-ID ${BuildConfig.API_KEY}")
+    @GET("/search/photos")
+    suspend fun searchImages(
+        @Query("page") page: Int,
+        @Query("per_page") per_page: Int
+    ): List<UnsplashImage>
+
+}
--- app/src/main/java/com/okihita/offlinecachingremotemediator/model/UnsplashImage.kt
+++ app/src/main/java/com/okihita/offlinecachingremotemediator/model/UnsplashImage.kt
@@ -2,12 +2,14 @@ package com.okihita.offlinecachingremotemediator.model
 
 import androidx.room.Embedded
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.okihita.offlinecachingremotemediator.util.Constants.UNSPLASH_IMAGE_TABLE
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Entity(tableName = UNSPLASH_IMAGE_TABLE)
 data class UnsplashImage(
+    @PrimaryKey(autoGenerate = false)
     val id: String,
     @Embedded
     val urls: Urls,
